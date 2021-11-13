8c89c56e0fa08390666970483554a12ad3d8ffec
kostaspapoutsisweb <30303325+kostaspapoutsisweb@users.noreply.github.com>
2021-11-13 23:20:34 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+if (navigator.serviceWorker.controller) {
+debug(
+    navigator.serviceWorker.controller.scriptURL +
+    ' (onload)', 'controller'
+    );
+    debug(
+    'An active service worker controller was found, ' +
+    'no need to register'
+    );
+} else {
+navigator.serviceWorker.register('service-worker.js', {
+    scope: './'
+    }).then(function(reg) {
+    debug(reg.scope, 'register');
+    debug('Service worker change, registered the service worker');
+    });
+}
+
+document.querySelector('#refresh').search = Date.now();
+
+function debug(message, element, append) {
+    var target = document.querySelector('#' + (element || 'log'));
+    target.textContent = message + ((append) ? ('/n' + target.textContent) : '');
+}
+
+document.getElementById('clearAndReRegister').addEventListener('click',
+  function() {
+    navigator.serviceWorker.getRegistration().then(function(registration) {
+      registration.unregister();
+      window.location.reload();
+    });
+  }
+);
